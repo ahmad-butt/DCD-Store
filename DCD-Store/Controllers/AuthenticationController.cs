9049f267dad87419d21c1927f733d84a8335caf9
ahamd-butt <hahahmad594@gmail.com>
2023-01-24 23:02:35 +0500

    Authentication Layout

---
+++ DCD-Store/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DCD_Store.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DCD_Store.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Signup()
+        {
+            return View("Signup");
+        }
+
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        // POST: /<controller>/
+        [HttpPost]
+        public IActionResult Signup(string? username, string? email, string? passwd, string? age, string? phone)
+        {
+            UserRepository userRepo = new UserRepository();
+            User user = userRepo.CreateUser(username, email, passwd, int.Parse(age), phone);
+            return View("Welcome", user);
+        }
+    }
+}
+
--- DCD-Store/Program.cs
+++ DCD-Store/Program.cs
@@ -22,7 +22,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Authentication}/{action=Signup}/{id?}");
 
 app.Run();
 
