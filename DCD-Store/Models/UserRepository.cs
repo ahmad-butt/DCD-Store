9b8cfc23a45930fe3ca48ac1cd1540da875d2ef2
ahamd-butt <hahahmad594@gmail.com>
2023-01-24 22:59:29 +0500

    Authentication Layout

---
+++ DCD-Store/Models/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DCD_Store.Models
+{
+	public class UserRepository
+	{
+		List<User> users = new List<User>();
+		public User CreateUser(string username, string email, string passwd, int age, string phone)
+		{
+			User newUser = new User
+            {
+                Id = 1,
+                Username = username,
+                Email = email,
+                Password = passwd,
+                Age = age,
+                Phone = phone
+            };
+            users.Add(newUser);
+            return newUser;
+		}
+	}
+}
+
