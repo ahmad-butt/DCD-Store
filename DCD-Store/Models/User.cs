83d8d2ce587044b8d9f892c4dd46ee597b72d9d8
ahamd-butt <hahahmad594@gmail.com>
2023-02-05 16:47:15 +0500

    Code First Approach Implemented

--- DCD-Store/Models/User.cs
+++ DCD-Store/Models/User.cs
@@ -5,15 +5,15 @@ namespace DCD_Store.Models;
 
 public partial class User
 {
-    public long? Id { get; set; }
+    public long ID { get; set; }
 
-    public string? Username { get; set; }
+    public string Username { get; set; }
 
-    public string? Email { get; set; }
+    public string Email { get; set; }
 
-    public string? Password { get; set; }
+    public string Password { get; set; }
 
-    public int? Age { get; set; }
+    public int Age { get; set; }
 
-    public string? Phone { get; set; }
+    public string Phone { get; set; }
 }
